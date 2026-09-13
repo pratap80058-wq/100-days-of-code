79a6b8486b54e92b1af0619371ffba0feee77bbc
pratap80058-wq <pratap80058@gmail.com>
2026-09-14 00:33:03 +0530

    Add files via upload

---
+++ q29.c
@@ -0,0 +1,15 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    long long factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+    printf("Factorial of %d = %lld\n", n, factorial);
+    return 0;
+}
