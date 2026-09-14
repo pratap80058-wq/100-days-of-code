ea252b77cd7c77cae7bf2503dbbfebc7237eea35
pratap80058-wq <pratap80058@gmail.com>
2026-09-14 07:35:18 +0530

    Add files via upload

---
+++ q33.c
@@ -0,0 +1,24 @@
+//Write a program to check if a number is an Armstrong number
+#include <stdio.h>
+int main()
+{
+    int num, original, remainder, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    original = num;
+    while(num != 0)
+    {
+        remainder = num % 10;
+        sum = sum + (remainder * remainder * remainder);
+        num = num / 10;
+    }
+    if(original == sum)
+    {
+        printf("Armstrong Number\n");
+    }
+    else
+    {
+        printf("Not an Armstrong Number\n");
+    }
+    return 0;
+}
