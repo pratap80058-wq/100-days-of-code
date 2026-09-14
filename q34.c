e7e62f3d142b14189201d8256d6968b2e54514d1
pratap80058-wq <pratap80058@gmail.com>
2026-09-14 07:35:31 +0530

    Add files via upload

---
+++ q34.c
@@ -0,0 +1,24 @@
+//Write a program to check if a number is prime.
+#include <stdio.h>
+int main()
+{
+    int num, i, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(i = 1; i <= num; i++)
+    {
+        if(num % i == 0)
+        {
+            count++;
+        }
+    }
+    if(count == 2)
+    {
+        printf("Prime Number\n");
+    }
+    else
+    {
+        printf("Not a Prime Number\n");
+    }
+    return 0;
+}
